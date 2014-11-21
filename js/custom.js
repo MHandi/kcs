5a933c1d6983f55bf89e16bdc9cbd1e5ae09c44f
MHandi <mylesmeng@gmail.com>
2014-11-21 12:56:28 +0000

    google analytics

--- js/custom.js
+++ js/custom.js
@@ -2,22 +2,22 @@
 // Custom JS (ADD YOUR JS HERE!)
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-$(document).ready(function(){
-	
+$(document).ready(function() {
+
 	/////////////////////////////////////////////////////////////////
 	// BACKSTRETCH SLIDER (UNCOMMENT TO USE)
 	/////////////////////////////////////////////////////////////////
-	
+
 	/*$("body").backstretch([
-		"img-1.jpg",
-		"img-2.jpg",
-		"img-3.jpg"
-	], {duration: 3000, fade: 750, random: true});*/
-	
+	 "img-1.jpg",
+	 "img-2.jpg",
+	 "img-3.jpg"
+	 ], {duration: 3000, fade: 750, random: true});*/
+
 	/////////////////////////////////////////////////////////////////
 	// CAMERA SLIDER
 	/////////////////////////////////////////////////////////////////
-	
+
 	if ($('.camera_slider').length > 0) {
 		jQuery('.camera_slider').camera({
 			fx: 'random',
@@ -27,79 +27,78 @@ $(document).ready(function(){
 			random: false
 		});
 	}
-	
+
 	/////////////////////////////////////////////////////////////////
 	// Fancybox
 	/////////////////////////////////////////////////////////////////
-	
+
 	//if ($('body#gallery').is('*')) { // Uncomment to run script on specific page only
-		if(jQuery.browser.mobile) {
-			var myPhotoSwipe = $(".fancybox").photoSwipe({ enableMouseWheel: false , enableKeyboard: false });
-		}
-		else {
-			// Single Image
-			//$("a.fancybox").fancybox();
-			
-			// Multiple Images
-			$("a.fancybox[rel='gallery_group']").fancybox({
-				'transitionIn'  :  'elastic',
-				'transitionOut' :  'elastic',
-				'speedIn'       :  600, 
-				'speedOut'      :  200 
-			});
-		}
-    //}
-	
+	if (jQuery.browser.mobile) {
+		var myPhotoSwipe = $(".fancybox").photoSwipe({enableMouseWheel: false, enableKeyboard: false});
+	}
+	else {
+		// Single Image
+		//$("a.fancybox").fancybox();
+
+		// Multiple Images
+		$("a.fancybox[rel='gallery_group']").fancybox({
+			'transitionIn': 'elastic',
+			'transitionOut': 'elastic',
+			'speedIn': 600,
+			'speedOut': 200
+		});
+	}
+	//}
+
 	/////////////////////////////////////////////////////////////////
 	// Animated Entrances
 	/////////////////////////////////////////////////////////////////
-	
-	$(window).scroll(function() {
-		$('.animatedEntrance').each(function(){
-		var imagePos = $(this).offset().top;
 
-		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+400) {
+	$(window).scroll(function () {
+		$('.animatedEntrance').each(function () {
+			var imagePos = $(this).offset().top;
+
+			var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow + 400) {
 				$(this).addClass("slideUp"); // slideUp, slideDown, slideLeft, slideRight, slideExpandUp, expandUp, fadeIn, expandOpen, bigEntrance, hatch
 			}
 		});
 	});
-	
+
 	/////////////////////////////////////////////////////////////////
 	// Form Validation
 	/////////////////////////////////////////////////////////////////
-	
-	$('#custom_form').formValidation({ 
-		validateText: ["name","message"],
+
+	$('#custom_form').formValidation({
+		validateText: ["name", "message"],
 		validateEmail: ["email"],
 		validateSpam: true,
 		captchaTheme: 'greyscale' // default, mini, dark, mini-dark, light, mini-light, greyscale, mini-greyscale
 	});
-	
+
 	/////////////////////////////////////////////////////////////////
 	// GOOGLE MAPS
 	/////////////////////////////////////////////////////////////////
 
 	function map() {
-		var myLatlng = new google.maps.LatLng(52.4740806,-1.8950115);
+		var myLatlng = new google.maps.LatLng(52.473851, -1.894977);
 		var mapOptions = {
-			zoom: 14,
+			zoom: 5,
 			center: myLatlng,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			scrollwheel: false,
-			styles:
-			[
-    	{
-        "featureType": "all",
-        "stylers": [
-            		{
-              	  	"saturation": -100
-            		},
-            		{
-                	"gamma": 0.5
-            			}
-        			]
-    			}
+			styles: [
+				{
+					"featureType": "all",
+					"stylers": [
+						{
+							"saturation": -100
+						},
+						{
+							"gamma": 0.5
+						}
+					]
+				}
 			]
 		}
 		// For Contact Page
@@ -111,28 +110,9 @@ $(document).ready(function(){
 				map: map,
 				icon: image
 			});
-			google.maps.event.addDomListener(window, 'resize orientationchange', function() {
+			google.maps.event.addDomListener(window, 'resize orientationchange', function () {
 				map.setCenter(myLatlng);
 			});
-		}
-		// For Footer
-		if ($('#footer-map').length > 0) {
-			var map = new google.maps.Map(document.getElementById('footer-map'), mapOptions);
-			var image = 'img/mapicon.png';
-			var marker = new google.maps.Marker({
-				position: myLatlng,
-				map: map,
-				icon: image
-			});
-			google.maps.event.addDomListener(window, 'resize orientationchange', function() {
-				map.setCenter(myLatlng);
-			}); 
 		}
 	}
-	google.maps.event.addDomListener(window, 'load', map);
-	
-	/////////////////////////////////////////////////////////////////
-	// YAY FOR LOLLIES!
-	/////////////////////////////////////////////////////////////////
-
-});
+})
