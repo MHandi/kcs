e4b384b7411c3326dab7b3c274d07089edc5646b
MHandi <mylesmeng@gmail.com>
2015-01-05 11:02:52 +0000

    new

--- email.php
+++ email.php
@@ -1,6 +1,11 @@
  <?php
                 require 'vendor/autoload.php';
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
                 /* USER CREDENTIALS
                 /  Fill in the variables below with your SendGrid
                 /  username and password.
@@ -15,28 +20,27 @@
                 $mail = new SendGrid\Email();
                 $mail->
                 setFrom("mylesmeng@gmail.com")->
-                addTo("ian@freshehr.com")->
                 addTo("mylesmeng@gmail.com")->
                 setSubject("Handihealth Account created ")->
-                setText("Hi Ian, An user has created an account by Stormpath database, please check api.stormpath.com")->
+                setText("Hi KCS! 有一个顾客发来留言")->
                 setHtml("<table style=\"border: solid 1px #000; background-color: #666; font-family: verdana, tahoma, sans-serif; color: #fff;\">
-<tr> <td> <h2>Hi Ian,</h2> <p>An user has created an account, please check.</p>
- <br> Cheers <br> HandiHealth CIC
-<p> <img src=\"http://handihealth.org/wp-content/uploads/2013/02/Header-RGB1.jpg\" alt=\"HandiHealth!\" /> </p></td> </tr> </table>");
-
+<tr> <td> <h2>Hi !</h2> <p>有一个顾客发来留言</p>
+ <br> Cheers <br> KCS BBQ
+</td> </tr> </table>");
+//<p> <img src=\"http://handihealth.org/wp-content/uploads/2013/02/Header-RGB1.jpg\" alt=\"HandiHealth!\" /> </p>
                 $response = $sendgrid->send($mail);
 
-                $token = ltrim($_SERVER["QUERY_STRING"], 'sptoken=');
-                $url = 'https://api.stormpath.com/emailVerificationTokens?sptoken=' . $token;
-                //https://api.stormpath.com/emailVerificationTokens?sptoken=4mrOjHDWdybREQmfTL00px
-                //open connection
-                $ch = curl_init();
-                //set the url, number of POST vars, POST data
-                curl_setopt($ch, CURLOPT_URL, $url);
-                //execute post
-                $result = curl_exec($ch);
-                //close connection
-                curl_close($ch);
+//                $token = ltrim($_SERVER["QUERY_STRING"], 'sptoken=');
+//                $url = 'https://api.stormpath.com/emailVerificationTokens?sptoken=' . $token;
+//                //https://api.stormpath.com/emailVerificationTokens?sptoken=4mrOjHDWdybREQmfTL00px
+//                //open connection
+//                $ch = curl_init();
+//                //set the url, number of POST vars, POST data
+//                curl_setopt($ch, CURLOPT_URL, $url);
+//                //execute post
+//                $result = curl_exec($ch);
+//                //close connection
+//                curl_close($ch);
 
 
 
